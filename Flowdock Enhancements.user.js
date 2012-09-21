111f4237a3288451286d9f6b09b20696d906b275
David Link <david@anedot.com>
2012-09-21 16:53:55 -0500

    Updates/Fixes/Time-formatter

--- Flowdock Enhancements.user.js
+++ Flowdock Enhancements.user.js
@@ -3,74 +3,108 @@
 // @namespace   http://fluidapp.com
 // @description Some Enhancements for Flowdock
 // @include     *
-// @author      Jens Arps http://jensarps.de
+// @author      Jens Arps http://jensarps.de, Modified by David Link
 // ==/UserScript==
 
 (function () {
 
-	var styleSheetId = 0;
+  var styleSheetId = 0;
 
-	var slice = Function.prototype.call.bind(Array.prototype.slice);
+  var slice = Function.prototype.call.bind(Array.prototype.slice);
 
-	var enhancements = {
 
-		applyUserImages: function(){
-			// generic styles:
-			var cssString = '#flowdock-chat .chat_line .chat_line_content { padding-left: 70px; } \
-							 #flowdock-chat .chat_line .nick { left: 70px; text-align: left; color: #A8A8A8; } \
-							 #flowdock-chat .chat_line .chat_line_header { padding: 10px 0 20px 0; } \
-							 #flowdock-chat .chat_line.hide_header .chat_line_header { padding-bottom: 0; } \
-							 #flowdock-chat #chat_app_lines .chat_line { padding-bottom: 5px; }';
-			this.createStylesheet(cssString);
+  var enhancements = {
 
-			// add images:
-			slice(document.querySelectorAll('#userlist_button div')).forEach(function(node){
-				var id = node.id.split('_').pop();
-				var img = (node && node.style.backgroundImage) || '';
+    applyUserImages: function(){
+      // generic styles:
+      var cssString = '#flowdock-chat .chat_line .chat_line_content { padding-left: 70px; } \
+               #flowdock-chat .chat_line .nick { text-align: left; color: #A8A8A8; padding: 0 0 0 50px !important; display: inline-block; position: relative !important; left: 35px !important; margin: 5px 0 -15px !important; height: 30px !important; } \
+               #flowdock-chat .chat_line .chat_line_header { padding: 5px 0 0 !important; } \
+               #flowdock-chat .chat_line.hide_header .chat_line_header { padding-bottom: 0 !important; } \
+               #flowdock-chat #chat_app_lines .chat_line { padding-bottom: 5px; }';
+      this.createStylesheet(cssString);
 
-				cssString = '.chat_line_header .nick.user_' + id + ':after { \
-					content: ""; \
-					background: ' + img + ' no-repeat center; \
-					width: 30px; \
-					height: 30px; \
-					display: inline-block; \
-					position: absolute; \
-					left: -50px; \
-					padding: 1px; \
-					border: solid 1px #BFBFBF; \
-				 }';
-				this.createStylesheet(cssString);
-			}, this);
-		},
+      // add images:
+      var fluid_this = this
+      setTimeout(function(){
+        $('#user_buttons .wrapper .avatar').each(function(){
+          var img = this.style.backgroundImage;
 
-		makeTimestampVisible: function(){
-	        var cssString = '#flowdock-chat .chat_line .timestamp span.time { display: block !important; }';
-	        this.createStylesheet(cssString);
-		},
-		
-		createStylesheet: function(cssText){
-			var head = document.getElementsByTagName('head')[0],
-			    style = document.createElement('style'),
-			    rules = document.createTextNode(cssText);
+          cssString = '.chat_line_header .nick { \
+            background: ' + img + ' no-repeat left center; \
+           }';
+          fluid_this.createStylesheet(cssString);
+        })
+        $('#userlist_button div').each(function(){
+          var id = $(this).attr('id').split('_').pop();
+          var img = this.style.backgroundImage;
 
-			style.type = 'text/css';
-			style.id="flowdockEnhancements_" + styleSheetId++;
-			if(style.styleSheet){
-			    style.styleSheet.cssText = rules.nodeValue;
-			}else{
-				style.appendChild(rules);
-			}
-			head.appendChild(style);
-		},
+          cssString = '.chat_line_header .nick.user_' + id + ' { \
+            background: ' + img + ' no-repeat left center; \
+           }';
+          fluid_this.createStylesheet(cssString);
+        })
+      }, 5000)
+    },
 
-		runAll: function(){
-			this.makeTimestampVisible();
-			this.applyUserImages();
-		}
-	};
+    timeFormatter: function(timestamp){
+      var time = timestamp.text().split(':')
+      var ampm = time[0] >= 12 ? "p" : "a";
+      if(time[0] > 12){
+        var hours = time[0] - 12;
+      } else if(time[0] == 0){
+        var hours = 12;
+      } else {
+        var hours = time[0]
+      }
+      var minutes = time[1];
+      timestamp.text(hours+':'+minutes+ampm);
+      timestamp.addClass('formatted');
+    },
+
+    formatTime: function(){
+      fluid_this = this;
+      setTimeout(function(){
+        $('#flowdock-chat .chat_line .timestamp span.time').each(function(){
+          fluid_this.timeFormatter($(this));
+        })
+        setInterval(function(){
+          $('#flowdock-chat .chat_line .timestamp span.time').not('.formatted').each(function(){
+            fluid_this.timeFormatter($(this))
+          })
+        }, 100)
+      }, 5000)
+    },
+
+    makeTimestampVisible: function(){
+          var cssString = '#flowdock-chat .chat_line .timestamp span.time { display: block !important; }';
+          this.createStylesheet(cssString);
+    },
+
+    createStylesheet: function(cssText){
+      var head = document.getElementsByTagName('head')[0],
+          style = document.createElement('style'),
+          rules = document.createTextNode(cssText);
+
+      style.type = 'text/css';
+      style.id="flowdockEnhancements_" + styleSheetId++;
+      if(style.styleSheet){
+          style.styleSheet.cssText = rules.nodeValue;
+      }else{
+        style.appendChild(rules);
+      }
+      head.appendChild(style);
+    },
+
+    runAll: function(){
+      this.makeTimestampVisible();
+      this.applyUserImages();
+      this.formatTime();
+    }
+  };
 
     if (window.fluid) {
-    	enhancements.runAll();
+      enhancements.runAll();
     }
 })();
 
--- Readme.md
+++ Readme.md
@@ -1,3 +1,12 @@
+Updates
+===
+
+**9/21/12**
+
+* Updated css to apply styles correctly in Fluid App.
+* Made a modification to correctly display the user's own avatar
+* Added a time formatter to switch from 24h time to 12h format.
+
 About
 ===
 
@@ -17,6 +26,8 @@ There's a bookmarklet you can drag into your bookmarks bar and just click after 
 The bookmarklet is embedded here: http://jensarps.github.com/flowdock-enhancements/
 
 If you're using Fluid, there's also a user script, it's the `Flowdock Enhancements.user.js` file.
+
+(To add a userscript to Fluid, click the script icon in the app's topbar)
 
 Screenshot
 ===
